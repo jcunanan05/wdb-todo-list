6af0c9e15721da315897900af15a1cbc6d3015a0
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-01-13 11:30:20 -0500

    Finish Deleting Todos
    I've learned event bubbling and countering it with stopPropagation() of jQuery :) I learned todo using the context of this on a callback

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,12 @@ $('li').click(function() {
   var li = $(this);
   li.toggleClass('completed');
 });
+
+//click X to delete todo
+$('ul span').click(function(event){
+  var li = $(this).parent();
+  li.fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
