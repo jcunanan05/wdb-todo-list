d45e3d9ff1ddb6e58188e6b0be8fad310a5ec29c
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-01-17 09:28:19 -0500

    Finish Creating todos
    Colt explained about the new children problem that the listener wont be added. So in jQuery he used the on() with extra parameters.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,30 @@
 //check todos by click
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
   var li = $(this);
   li.toggleClass('completed');
 });
 
 //click X to delete todo
-$('ul span').click(function(event){
+$('ul').on('click', 'span', function(event){
   var li = $(this).parent();
   li.fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+
+//select input
+$('input[type="text"]').keypress(function (){
+  var enterKey = 13;
+  if(event.which === enterKey) {
+    //new todo Text
+    var todoText = $(this).val();
+    //add to ul
+    $('ul').append(`
+      <li> <span>X</span> ${todoText} </li>
+    `);
+    
+    $(this).val('');
+  }
 });
